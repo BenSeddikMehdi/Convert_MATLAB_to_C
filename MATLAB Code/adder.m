514715209cbe459063a9cfabb51bc8116b8fd8f1
Mehdi <amigoinpt@gmail.com>
2019-09-20 14:07:10 +0100

    Add files via upload

---
+++ MATLAB Code/adder.m
@@ -0,0 +1,9 @@
+function c = adder(a,b)
+%ADDER Summary of this function goes here
+%   Detailed explanation goes here
+fprintf('Hello Jayef\n');
+c = a + b;
+fprintf('c = %d\n',c);
+fprintf('Hola Jayef\n');
+end
+
